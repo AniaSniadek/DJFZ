f7cfb2060ca5c05bebe79be351670f02f7296989
Ania Sniadek <sniadek97@gmail.com>
2020-03-17 17:34:42 +0100

    regular expressions finish

--- cw1/zad.py
+++ cw1/zad.py
@@ -1,12 +1,12 @@
 import sys
 
 tablica = []
-space = " "
+spacja = " "
 
 for line in sys.stdin:
 	line = line.rstrip('\n')
 	count = len(line)
-	result = str(count) + space + line
+	result = str(count) + spacja + line
 	tablica.append(result)
 
 for x in tablica:
